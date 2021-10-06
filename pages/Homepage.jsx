c2822c91a0bde8fd4d7b382f45f163461fa49d04
hrithik73 <hrithiksinghvps_cse18@its.edu.in>
2021-10-06 21:14:56 +0530

    Samall Changes

--- pages/Homepage.jsx
+++ pages/Homepage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
     <>
       <div className="relative bg-white overflow-hidden">
         <div className="max-w-full mx-auto">
-          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-28 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-28 lg:max-w-2xl lg:w-full lg:pb-28  xl:pb-32">
             <Header />
             <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
@@ -31,8 +31,8 @@ const HomePage = () => {
         </div>
         <div className="lg:absolute lg:h-full lg:inset-y-0 lg:right-0 lg:w-1/2 ">
           <img
-            className="h-full w-full object-contain lg:object-scale-down sm:h-72 md:h-full lg:w-full "
-            className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+            // className="h-full w-full object-contain lg:object-scale-down sm:h-72 md:h-full lg:w-full "
+            // className="transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
             src="dbrave.png"
             alt=""
           />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { team } from "../data/data"
+
 import { MdEmail } from "react-icons/md"
 import { FaPhoneAlt } from "react-icons/fa"
 
@@ -8,7 +10,7 @@ const Footer = () => {
     <>
       <div className="flex  justify-center px-8 py-3 my-5">
         <div className="self-center border-2 border-red-200 max-w-md bg-white py-2 px-6 sm:py-4 sm:px-10 shadow-lg rounded-lg">
-          <p>Contact Me</p>
+          <p>Contact Me at </p>
           <a
             className="flex gap-x-2 text-blue-700"
             href="mailto:dbraveofficial@gmail.com"
@@ -33,8 +35,13 @@ const Footer = () => {
           </p>
         </div>
       </div>
-      <div className="flex justify-center 	border-black-200 pb-1">
-        <a> © 2021 | Coded with ❤️ by Hrithik </a>
+      <div
+        className="flex justify-center border-black-200 pb-1 cursor-pointer"
+        onClick={() => {
+          window.open(team.developer.insta)
+        }}
+      >
+        <a className="text-sm"> © 2021 | Coded with ❤️ by Hrithik </a>
       </div>
     </>
   )

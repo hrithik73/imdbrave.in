162f1cb8a9c1daa2e4b5806a7b2dfda2d5ecc13f
hrithik73 <hrithiksinghvps_cse18@its.edu.in>
2021-11-12 12:32:40 +0530

    Small Improvements

--- src/components/Recents/RecentSongCard.jsx
+++ src/components/Recents/RecentSongCard.jsx
@@ -4,10 +4,10 @@ import { FaPlay } from "react-icons/fa"
 
 const RecentSongCard = ({ item }) => {
   return (
-    <div className="w-full sm:w-1/2 md:w-1/3 flex flex-col p-3">
+    <div className="w-full justify-center sm:w-1/2 md:w-1/3 flex flex-col p-3">
       <div className="bg-white rounded-lg shadow-lg overflow-hidden flex-1 flex flex-col">
         <div
-          className="bg-cover h-48 w-48"
+          className="bg-cover lg:h-full lg:w-full h-48 w-48"
           // style={{
           //   backgroundImage: `url(${item.imgURL})`,
           // }}
@@ -21,13 +21,13 @@ const RecentSongCard = ({ item }) => {
             layout="responsive"
           />
         </div>
-        <div className="p-4 flex-1 flex flex-col">
-          <h3 className="mb-4 text-2xl">{item.title}</h3>
+        <div className="p-4 flex-1 flex  flex-col">
           <div className="mb-4 text-grey-darker text-sm flex-1">
+            <h3 className="mb-4 text-2xl">{item.title}</h3>
             <p>{item.artist}</p>
           </div>
           <button
-            className="flex mx-3 px-3 py-2 w-full bg-red-600 text-white text-xs justify-center font-bold uppercase rounded"
+            className="flex mx-3 px-3 py-2 w-full bg-red-600 text-white text-xs justify-center self-center font-bold uppercase rounded"
             onClick={() => {
               window.open(item.link)
             }}

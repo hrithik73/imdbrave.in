262a88db721fca4474dd55bd21b870529d92af4c
hrithik73 <hrithiksinghvps_cse18@its.edu.in>
2022-01-10 19:48:42 +0530

    Test

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,9 @@ import HomePage from './Homepage'
 import 'tailwindcss/tailwind.css';
 import "react-image-gallery/styles/css/image-gallery.css";
 
-
 const App = () => {
-
   return (
-    <>
       <HomePage />
-    </>
   )
 }
 export default App

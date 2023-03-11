9c12ef5ae1e3d13e205cf6b4a835b29c76c00b15
Hritik Singh <48889727+hrithik73@users.noreply.github.com>
2023-03-11 11:47:55 +0530

    feat: heading changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,9 @@
-import 'tailwindcss/tailwind.css';
-import 'react-image-gallery/styles/css/image-gallery.css';
+// import 'tailwindcss/tailwind.css';
+// import 'react-image-gallery/styles/css/image-gallery.css';
 import Head from 'next/head';
-import ImageGallery from 'react-image-gallery';
 
 import Atropos from 'atropos/react';
 import 'atropos/css';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
@@ -53,10 +50,13 @@ const HomePage = () => {
           <main className='mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28'>
             <div className='sm:text-center lg:text-left'>
               <h1 className='text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl'>
-                <span className='block xl:inline'>Hey! I am </span>{' '}
-                <span className='block text-indigo-600 xl:inline'>DBRAVE</span>
+                <span className='block xl:inline'>Hey,</span>
+                <span className='bg-gradient-to-r from-fuchsia-600 to-purple-600 bg-clip-text text-transparent'>
+                  {' '}
+                  This is DBRAVE
+                </span>
               </h1>
-              <p className='mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0 '>
+              <p className='mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-md lg:mx-0 '>
                 Md. Danish Iqbal, who goes with his stage name "Dbrave" is an
                 Indian singer and rapper from Darbhanga(Bihar). Dbrave is one of
                 the biggest independent artists from Bihar having a huge
@@ -73,10 +73,10 @@ const HomePage = () => {
         </div>
         <div className='lg:absolute lg:inset-y-0 lg:right-0  lg:w-1/2 lg:top-0'>
           <Atropos
-            // style={{
-            //   width: '500px',
-            //   height: '500px',
-            // }}
+            style={{
+              // width: '500px',
+              height: '100%',
+            }}
             activeOffset={40}
             shadowScale={1.05}
             onEnter={() => console.log('Enter')}
